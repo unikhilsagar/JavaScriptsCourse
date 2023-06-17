eb28a65862eafb378ecb26aa4612186a62fcc66f
Utnoor Nikhil Sagar <unikhilsagar@gmail.com>
2023-06-17 18:36:14 +0530

    deep copy changes

--- shallowAndDeep.js
+++ shallowAndDeep.js
@@ -3,6 +3,8 @@ var a = {
     name:'nikhil',
     skill:'front'
 }
+
+//deep copy example
 var b = {...a};
 console.log(a===b)
 b.name = 'sai charan'
@@ -11,3 +13,13 @@ b.skill = 'testing'
 console.log(a===b)
 
 console.log('a = ',a,'b=',b)
+
+//JSON parse 
+let ingredients_list = ["noodles", { list: ["eggs", "flour", "water"] }];
+let ingredients_list_deepcopy = JSON.parse(JSON.stringify(ingredients_list));
+
+// Change the value of the 'list' property in ingredients_list_deepcopy.
+ingredients_list_deepcopy[1].list = ["rice flour", "water"];
+// The 'list' property does not change in ingredients_list.
+console.log(ingredients_list[1].list);
+// Array(3) [ "eggs", "flour", "water" ]
